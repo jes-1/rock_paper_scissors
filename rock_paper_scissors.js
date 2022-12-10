192969609271dab66e3154db34afb83c8d50bf29
Jesica <sweetjesi03@gmail.com>
2022-12-10 11:24:48 -0800

    add computer choice

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,5 +1,15 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function getComputerChoice(max) {
+   let randomNumber =  Math.floor(Math.random() * max);
+   console.log(randomNumber);
+   if (randomNumber === 0) {
+    return "rock"
+   } else if (randomNumber === 1) {
+    return "paper"
+   } else if (randomNumber === 2) {
+    return "scissors"
+   }
+    return;
 }
 
-console.log(getRandomInt(3));
+console.log(getComputerChoice(3));
+
