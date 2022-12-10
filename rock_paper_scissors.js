4e478a70d6ef4a2dade4d12380e51e9172050595
Jesica <sweetjesi03@gmail.com>
2022-12-10 12:49:59 -0800

    add playRound function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,6 +1,5 @@
-function getComputerChoice(max) {
-   let randomNumber =  Math.floor(Math.random() * max);
-   console.log(randomNumber);
+function getComputerChoice() {
+   let randomNumber =  Math.floor(Math.random() * 3);
    if (randomNumber === 0) {
     return "rock"
    } else if (randomNumber === 1) {
@@ -8,8 +7,35 @@ function getComputerChoice(max) {
    } else if (randomNumber === 2) {
     return "scissors"
    }
-    return;
 }
 
-console.log(getComputerChoice(3));
+function getPlayerSelection () {
+    let choice = prompt("Rock, Paper, or Scissors?");
+    return choice;
+}
 
+
+function playRound(myAnswer, computerAnswer){
+    console.log(myAnswer)
+    console.log(computerAnswer)
+    if (myAnswer === computerAnswer) {
+        return "draw";
+    } else if (myAnswer === "rock" && computerAnswer === "paper") {
+        return "computer wins";
+    } else if (myAnswer === "rock" && computerAnswer === "scissors") {
+        return "player wins";
+    } else if (myAnswer === "paper" && computerAnswer === "rock") {
+        return "player wins";
+    } else if (myAnswer === "paper" && computerAnswer ==="scissors") {
+        return "computer wins";
+    } else if (myAnswer === "scissors" && computerAnswer === "rock") {
+        return "computer wins";
+    } else if (myAnswer === "scissors" && computerAnswer === "paper") {
+        return "player wins";
+    }
+
+}
+
+let playerSelection = getPlayerSelection()
+let computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection));
