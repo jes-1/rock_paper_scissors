f03383ea7c054ff3494303d1e826bcacba1468bc
Jesica <sweetjesi03@gmail.com>
2022-12-10 22:14:01 -0800

    add game function to loop game

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,21 +1,21 @@
 function getComputerChoice() {
-   let randomNumber =  Math.floor(Math.random() * 3);
-   if (randomNumber === 0) {
-    return "rock"
-   } else if (randomNumber === 1) {
-    return "paper"
-   } else if (randomNumber === 2) {
-    return "scissors"
-   }
+    let randomNumber = Math.floor(Math.random() * 3);
+    if (randomNumber === 0) {
+        return "rock"
+    } else if (randomNumber === 1) {
+        return "paper"
+    } else if (randomNumber === 2) {
+        return "scissors"
+    }
 }
 
-function getPlayerSelection () {
+function getPlayerSelection() {
     let choice = prompt("Rock, Paper, or Scissors?");
     return choice;
 }
 
 
-function playRound(myAnswer, computerAnswer){
+function playRound(myAnswer, computerAnswer) {
     console.log(myAnswer)
     console.log(computerAnswer)
     if (myAnswer === computerAnswer) {
@@ -26,23 +26,42 @@ function playRound(myAnswer, computerAnswer){
         return "player wins";
     } else if (myAnswer === "paper" && computerAnswer === "rock") {
         return "player wins";
-    } else if (myAnswer === "paper" && computerAnswer ==="scissors") {
+    } else if (myAnswer === "paper" && computerAnswer === "scissors") {
         return "computer wins";
     } else if (myAnswer === "scissors" && computerAnswer === "rock") {
         return "computer wins";
     } else if (myAnswer === "scissors" && computerAnswer === "paper") {
         return "player wins";
     }
-
 }
 
 
+function game(){
 
-
-
-for (let i = 0; i <5; i++) {
-    let playerSelection = getPlayerSelection()
-    let computerSelection = getComputerChoice()
-    console.log(playRound(playerSelection, computerSelection));
+    let computerScore = 0
+    let playerScore = 0
+    
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = getPlayerSelection()
+        let computerSelection = getComputerChoice()
+        let winning = playRound(playerSelection, computerSelection);
+        if (winning === "computer wins") {
+            computerScore++;
+            console.log("comp:" + computerScore);
+        } else if (winning === "player wins") {
+            playerScore++;
+            console.log("player:" + playerScore);
+        }
+    }
+    
+    
+    if(computerScore > playerScore) {
+        console.log("computer wins!")
+    } else if(playerScore > computerScore) {
+        console.log("player wins!")
+    } else if (computerScore === playerScore) {
+        console.log("it's a tie!")
+    }
 }
 
+game();
